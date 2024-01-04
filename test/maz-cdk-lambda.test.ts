2413c3d0cf9fdaf774653b3de5826b14468695b8
mazina <mn.usyd@gmail.com>
2024-01-05 06:55:54 +1100

    Initial commit

---
+++ test/maz-cdk-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MazCdkLambda from '../lib/maz-cdk-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/maz-cdk-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MazCdkLambda.MazCdkLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
