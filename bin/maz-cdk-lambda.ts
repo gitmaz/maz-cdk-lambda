887210e4a9fae757a43318bfd52ecfb7d436aa7c
Maziar Navabi <mn.usyd@gmail.com>
2024-01-07 22:38:16 +1100

    set account and zone

--- bin/maz-cdk-lambda.ts
+++ bin/maz-cdk-lambda.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MazCdkLambdaStack } from '../lib/maz-cdk-lambda-stack';
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
-new MazCdkLambdaStack(app, 'MazCdkLambdaStack', {
+new CdkWorkshopStackStack(app, 'CdkWorkshopStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new MazCdkLambdaStack(app, 'MazCdkLambdaStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+   env: { account: '154501673607', region: 'ap-southeast-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
